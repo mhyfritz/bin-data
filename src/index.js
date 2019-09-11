7ebed0f471168db1e9d737f859af461189302b81
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2019-09-11 07:07:58 +0200

    add dedicated `chunk` function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import * as d3Array from "d3-array";
 
-export { bin, d3Array as pick };
+export { bin, chunk, d3Array as pick };
 
 function bin(data, numBins, pickRepresentative = d3Array.max) {
   if (!Array.isArray(data)) {
     data = Array.from(data);
   }
 
-  const binned = new Array(numBins);
+  const chunks = chunk(data, numBins);
+  return chunks.map(chunk => pickRepresentative(chunk.data));
+}
+
+function chunk(data, numBins) {
+  const chunks = new Array(numBins);
   const binSize = data.length / numBins;
 
   for (let i = 0, start = 0, end = 0; i < numBins; i += 1) {
     end = Math.round((i + 1) * binSize);
-    const chunk = data.slice(start, end);
-    binned[i] = pickRepresentative(chunk);
+    const slice = data.slice(start, end);
+    chunks[i] = {
+      start,
+      end: end - 1,
+      data: slice
+    };
     start = end;
   }
 
-  return binned;
+  return chunks;
 }
